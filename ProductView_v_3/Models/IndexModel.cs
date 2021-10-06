ca2c3b674ea4166cd05ccfd1bf1866adb5e0d5bc
Никита Куваев <likeaboss13371@gmail.com>
2021-10-06 20:33:16 +0300

    Init

---
+++ ProductView_v_3/Models/IndexModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductView_v_3.Models
+{
+    public class IndexModel
+    {
+        public List<Product> products = ProductRepository.GetProducts();
+    }
+}
